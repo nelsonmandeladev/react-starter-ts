671417205d8008ccaf89654d923a2f324db5437c
nelsonmandeladev <patricknelson1995@gmail.com>
2023-02-23 06:04:43 +0100

    Project landing added

--- src/components/BasComponent.tsx
+++ src/components/BasComponent.tsx
@@ -1,6 +1,5 @@
-import { useState } from 'react';
-import {useAppDispatch, useAppSelector} from "../app/hooks";
-import {baseReducer, baseStateSelector, baseReducerWithAction} from "../slices/baseSlice";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { baseReducer, baseStateSelector, baseReducerWithAction } from "../slices/baseSlice";
 
 
 export function BaseComponent() {
@@ -9,17 +8,63 @@ export function BaseComponent() {
     const dispatch = useAppDispatch();
     return (
         <div className="App">
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-                <a
-                    onClick={() => dispatch(baseReducer())}
-                    href="#"
-                    className="bg-blue px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                    Dispatch and Action
-                </a>
-                <a href="#" className="text-base font-semibold leading-7 text-gray-900">
-                    {baseStateValue}
-                </a>
+            <div className="relative px-6 lg:px-8">
+                <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+                    <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+                        <div className="relative py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                            Clone the base project from here.{' '}
+                            <a href="https://github.com/nelsonmandeladev/react-starter-ts" className="font-semibold text-indigo-600">
+                                <span className="absolute inset-0" aria-hidden="true" />
+                                Get the code <span aria-hidden="true">&rarr;</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="text-center">
+                        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                            React base project with <span className="text-blue">TypeScript</span>
+                        </h1>
+                        <p className="mt-6 text-lg leading-8 text-gray-600">
+                            This is simple starter react project to help you dive directly into the dev stage of your react app by skipping fastidious setup and configuration stage
+                        </p>
+                        <div className="mt-10 flex items-center justify-center gap-x-6">
+                            <a
+                                href="#"
+                                onClick={() => dispatch(baseReducer())}
+                                className="bg-blue px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Dispatch and action
+                            </a>
+                            <a href="#" className="text-sm font-semibold leading-6 text-gray-900 shadow-md p-4">
+                                {baseStateValue}
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+                    <svg
+                        className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+                        viewBox="0 0 1155 678"
+                    >
+                        <path
+                            fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
+                            fillOpacity=".3"
+                            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+                        />
+                        <defs>
+                            <linearGradient
+                                id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
+                                x1="1155.49"
+                                x2="-78.208"
+                                y1=".177"
+                                y2="474.645"
+                                gradientUnits="userSpaceOnUse"
+                            >
+                                <stop stopColor="#121569" />
+                                <stop offset={1} stopColor="#121569" />
+                            </linearGradient>
+                        </defs>
+                    </svg>
+                </div>
             </div>
         </div>
     )
